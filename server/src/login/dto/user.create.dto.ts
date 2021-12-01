874115646e4fd6a0bc67511fd0150cf3b920c17d
Christopher Bartel <christopher.bartel@cologne-intelligence.de>
2021-12-01 20:24:13 +0100

    fix(#31): users could not use spaces in their character names

--- server/src/login/dto/user.create.dto.ts
+++ server/src/login/dto/user.create.dto.ts
@@ -1,8 +1,10 @@
-import { IsAlpha, IsNotEmpty } from 'class-validator';
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
 import { CreateUser } from '@nw-company-tool/model';
 
 export class CreateUserDto implements CreateUser {
-  @IsAlpha()
-  @IsNotEmpty()
+  @Matches(/^[a-zA-Z0-9\s]+$/)
+  @MinLength(2)
+  @MaxLength(24)
+  @IsString()
   characterName: string;
 }
